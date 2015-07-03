bf9763f47102a0bc369e81ba5d672de9c67eafaf
sunshinezzz <173509185@qq.com>
2015-07-03 15:04:20 +0800

    test

--- community/urls.py
+++ community/urls.py
@@ -1,4 +1,4 @@
-"""datacenter URL Configuration
+"""community URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -18,5 +18,4 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^dm/',include('data_mining.urls'))
 ]
